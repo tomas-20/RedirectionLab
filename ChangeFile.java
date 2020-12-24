db5a041c175d3e3deb82f58b03775542d53c2b92
tomas-20 <tacuna20@stuy.edu>
2020-12-24 01:06:48 -0500

    setteled on the least stupid way to make the file scanning thing

---
+++ ChangeFile.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ChangeFile {
+  public static void changeFile(boolean type) {
+    Scanner file = new Scanner(System.in);
+    boolean hasNextLine = file.hasNextLine();
+    while (hasNextLine) {
+      Scanner line = new Scanner(file.nextLine());
+      boolean hasNext = line.hasNext();
+      while (hasNext) {
+        String next = line.next();
+        if (type) {
+          next = MakeStars.stars(next);
+        }
+        System.out.print(next);
+        hasNext = line.hasNext();
+        if (hasNext) {
+          System.out.print(" ");
+        }
+      }
+      hasNextLine = file.hasNextLine();
+      if (hasNextLine) {
+        System.out.println();
+      }
+    }
+  }
+}
--- MakeStars.java
+++ MakeStars.java
@@ -1,9 +1,12 @@
-public class MakeStars {
+public class MakeStars extends FileChanger {
   public static String stars(String s) {
     String output = "";
     for (int i = 0; i < s.length(); i ++) {
       output += "*";
     }
     return output;
+  }
+  public static void main(String[] args) {
+    ChangeFile.changeFile(true);
   }
 }
