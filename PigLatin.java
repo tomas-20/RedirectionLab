c463cefef3dd115c13d8d109f34f81a59432b880
tomas-20 <tacuna20@stuy.edu>
2020-12-26 17:47:23 -0500

    added the intermediate pig latin

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,6 @@
 public class PigLatin {
   private static String[] vowels = {"a", "e", "i", "o", "u"};
+  private static String[] doubles = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
   private static boolean in(String a, String[] b) {
     for (int i = 0; i < b.length; i ++) {
       if (a.equals(b[i])) {
@@ -10,11 +11,19 @@ public class PigLatin {
   }
   public static String pigLatinSimple(String s) {
     String first = s.substring(0, 1);
-    if(in(first, vowels)) {
+    if (in(first, vowels)) {
       return s + "hay";
     }
-    else {
-      return s.substring(1) + first + "ay";
+    return s.substring(1) + first + "ay";
+  }
+  public static String pigLatin(String s) {
+    if (s.length() == 1) {
+      return pigLatinSimple(s);
     }
+    String start = s.substring(0, 2);
+    if (in(start, doubles)) {
+      return s.substring(2) + start + "ay";
+    }
+    return pigLatinSimple(s);
   }
 }
